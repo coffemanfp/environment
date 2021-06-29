7705ddf3325f939d62b4a8c60a060f2d3710cd8e
Arturo Nava <arthurnavah@gmail.com>
2021-06-29 18:33:51 -0500

    update

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ if [ ! -x ./install.sh ]; then
 fi
 
 main() {
+    # check install all
     if [ "$INSTALL_ALL" == 1 ]; then
         INSTALL_EDITOR=1
         INSTALL_CONSOLE=1
@@ -13,23 +14,50 @@ main() {
 
     if [ "$INSTALL_EDITOR" != 1 ] && [ "$INSTALL_CONSOLE" != 1 ]; then
         echo "List of installations:"
+        echo -e "\t all"
         echo -e "\t editor"
         echo -e "\t console"
         exit 1
     fi
 
+    # check requireds
+    if [ "$INSTALL_EDITOR" == 1 ]; then
+        if [ "$NO_PROVIDERS" != 1 ]; then
+            requiredCommands go node python python2 python3
+        else
+            requiredCommands go
+        fi
+
+        requiredSudoCommands apt npm apt-key tee
+
+        #--- Check Go Env
+        if [ -z "$(go env GOROOT)" ]; then
+            echo "[Editor Installer] : The GOROOT environment variable is not set"
+            exit 1
+        fi
+        if [ -z "$(go env GOPATH)" ]; then
+            echo "[Editor Installer] : The GOPATH environment variable is not set"
+            exit 1
+        fi
+    fi
+    if [ "$INSTALL_CONSOLE" == 1 ]; then
+        requiredCommands curl wget git
+        requiredSudoCommands snap apt 
+    fi
+
+    # installs
     if [ "$INSTALL_EDITOR" == 1 ]; then
         if [ "$NO_PROVIDERS" == 1 ]; then
-            echo "Install Editor without Providers..."
+            echo "[Editor Installer] : Install Editor without Providers..."
         else
-            echo "Install Editor..."
+            echo "[Editor Installer] : Install Editor..."
         fi
 
         installEditor
     fi
 
     if [ "$INSTALL_CONSOLE" == 1 ]; then
-        echo "Install Console and Tools..."
+        echo "[Console Installer] : Install Console and Tools..."
 
         installConsole
     fi
@@ -69,6 +97,9 @@ for p in "$@"; do
         INSTALL_ALL=1
     fi
 done;
+if [ "$#" == 0 ]; then
+    INSTALL_ALL=1
+fi
 
 # requiredCommands terminates the execution if there are no commands
 requiredCommands() {
@@ -104,29 +135,13 @@ requiredSudoCommands() {
 
 # installEditor install editor
 installEditor() {
-    if [ "$NO_PROVIDERS" != 1 ]; then
-        requiredCommands go node python python2 python3
-    else
-        requiredCommands go
-    fi
-
-    requiredSudoCommands apt npm apt-key tee
-
-    #--- Check Go Env
-    if [ -z "$(go env GOROOT)" ]; then
-        echo "The GOROOT environment variable is not set"
-        exit 1
-    fi
-    if [ -z "$(go env GOPATH)" ]; then
-        echo "The GOPATH environment variable is not set"
-        exit 1
-    fi
-
     echo "dark" > ~/.mode
 
     sudo apt install curl git xclip snap snapd
+
     if [ "$NO_PROVIDERS" != 1 ]; then
-        echo "Add yarn key, Update yarn"
+        echo ""
+        echo "[Editor Installer] : Adding yarn key, Updating yarn..."
         curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - &>/dev/null
 
         echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
@@ -134,7 +149,7 @@ installEditor() {
         sudo apt update 1>/dev/null
 
         sudo apt install python3-neovim python3-pip python3-dev python-setuptools python3-setuptools ruby-dev automake autoconf autotools-dev build-essential perl cpanminus yarn
-        echo "----------------------"
+        echo "[Editor Installer] : ----------------------"
 
         requiredCommands pip pip3 gem
         requiredSudoCommands gem cpanm
@@ -143,7 +158,8 @@ installEditor() {
     requiredCommands curl git
     requiredSudoCommands snap apt 
 
-    echo "Removing current version of Vim and NeoVim"
+    echo ""
+    echo "[Editor Installer] : Removing current version of Vim and NeoVim..."
     sudo apt remove --purge vim 2>/dev/null
     sudo apt remove --purge vim-editor 2>/dev/null
     sudo apt remove --purge neovim 2>/dev/null
@@ -152,17 +168,19 @@ installEditor() {
     sudo snap remove --purge vim 2>/dev/null
     sudo snap remove --purge nvim 2>/dev/null
     sudo snap remove --purge neovim 2>/dev/null
-    echo "----------------------"
+    echo "[Editor Installer] : ----------------------"
 
-    echo "Install Vim and NeoVim"
+    echo ""
+    echo "[Editor Installer] : Installing Vim and NeoVim..."
     sudo snap install vim-editor --beta
     sudo snap install nvim --classic
-    echo "----------------------"
+    echo "[Editor Installer] : ----------------------"
 
     requiredCommands nvim
 
     if [ "$NO_PROVIDERS" != 1 ]; then
-        echo "Install Providers for NeoVim"
+        echo ""
+        echo "[Editor Installer] : Installing Providers for NeoVim..."
         sudo npm install -g neovim
         sudo gem install neovim
         pip install -U pynvim 
@@ -173,38 +191,40 @@ installEditor() {
         pip3 install -U msgpack-python
         python3 -mpip install --user -U msgpack
         yarn install --froken-lockfile
-        echo "----------------------"
+        echo "[Editor Installer] : ----------------------"
     fi
 
-    echo "Creating folders"
+    echo ""
+    echo "[Editor Installer] : Creating folders..."
     mkdir ~/.vim 2>/dev/null
     mkdir -p ~/.vim/tmp 2>/dev/null
     sudo rm -r ~/.vim/bundles
     mkdir -p ~/.vim/bundles 2>/dev/null
     mkdir -p ~/.config/nvim 2>/dev/null
-    echo "----------------------"
 
-    echo " - Copying configuration VIM"
+    echo "[Editor Installer] : - Copying configuration VIM..."
     cp ./config/vimrc ~/.vim/.
 
-    echo "- Copying configuration NeoVim"
+    echo "[Editor Installer] : - Copying configuration NeoVim..."
     cp ./config/init.vim ~/.config/nvim/.
 
-    echo "- Copying configuration coc.nvim"
+    echo "[Editor Installer] : - Copying configuration coc.nvim..."
     cp ./config/coc-settings.json ~/.config/nvim/.
 
-    echo "----------------------"
-    echo "Configuring tools GIT with Vim"
+    echo "[Editor Installer] : ----------------------"
+
+    echo ""
+    echo "[Editor Installer] : Configuring tools GIT with Vim..."
     git config --global core.editor nvim
     git config --global diff.tool vimdiff
     git config --global merge.tool vimdiff
-    echo "----------------------"
+    echo "[Editor Installer] : ----------------------"
 
-    echo "----------------------"
-    echo "Install Vim Plugin Manager"
+    echo ""
+    echo "[Editor Installer] : Install Vim Plugin Manager..."
     curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh 2>/dev/null > ~/.vim/bundles/installer.sh
     sh ~/.vim/bundles/installer.sh ~/.vim/bundles
-    echo "----------------------"
+    echo "[Editor Installer] : ----------------------"
 
     nvim --headless +"call dein#install()" +qall
     nvim --headless +"call dein#update()" +qall
@@ -214,68 +234,71 @@ installEditor() {
     nvim --headless +GoUpdateBinaries +qall
     nvim --headless +CocUpdate +qall
 
-    echo "+ Installation Editor successful! +"
+    echo ""
+    echo "[Editor Installer] : + Installation Editor successful! +"
 }
 
 # installConsole install console and tools
 installConsole() {
-    requiredCommands curl wget git
-    requiredSudoCommands snap apt 
+    echo "dark" > ~/.mode
 
     sudo snap install alacritty --classic
 
-    echo "Install tools"
+    echo ""
+    echo "[Console Installer] : Installing tools..."
     sudo apt install curl git tmux cmus zsh git-flow shellcheck exiftool rar fzf
-    echo "----------------------"
+    echo "[Console Installer] : ----------------------"
 
-    echo "dark" > ~/.mode
-
-    echo "Add yarn key, Update yarn"
+    echo ""
+    echo "[Console Installer] : Adding yarn key, Updating yarn"
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - &>/dev/null
 
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
     sudo apt update 1>/dev/null
-
     sudo apt install yarn 1>/dev/null
-    echo "----------------------"
+    echo "[Console Installer] : ----------------------"
 
-    echo "Download Tmux Plugin Manager"
+    echo ""
+    echo "[Console Installer] : Downloading Tmux Plugin Manager..."
     sudo rm -r ~/.tmux/plugins 
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 1>/dev/null
-    echo "----------------------"
 
-    echo "- Copying configuration Alacritty"
+    echo "[Console Installer] : - Copying configuration Alacritty..."
     mkdir -p ~/.config/alacritty 2>/dev/null
     cp ./config/alacritty.yml ~/.config/alacritty/.
 
-    echo "- Copying configuration Tmux"
+    echo "[Console Installer] : - Copying configuration Tmux..."
     cp ./config/.tmux.conf ~/.
+    echo "[Console Installer] : ----------------------"
 
-    echo "Install Tmux Plugins"
+    echo ""
+    echo "[Console Installer] : Installing Tmux Plugins..."
     bash ~/.tmux/plugins/tpm/scripts/install_plugins.sh
-    echo "----------------------"
+    echo "[Console Installer] : ----------------------"
 
-    echo "Install Oh My ZSH!"
+    echo ""
+    echo "[Console Installer] : Installing Oh My ZSH!..."
     curl -L http://install.ohmyz.sh 2>/dev/null | sh
     wget --no-check-certificate http://install.ohmyz.sh -O - | sh
-    echo "----------------------"
 
-    echo "Configuring Zsh Plugins"
+    echo ""
+    echo "[Console Installer] : ----------------------"
+    echo "[Console Installer] : Configuring Zsh Plugins..."
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting 1>/dev/null
-
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions 1>/dev/null
-    echo "----------------------"
 
-    echo "- Copying configuration Zsh"
+    echo "[Console Installer] : - Copying configuration Zsh..."
     cp ./config/.zshrc ~/. 
+    echo "[Console Installer] : ----------------------"
 
-    echo "----------------------"
-    echo "Configuring ZSH"
+    echo ""
+    echo "[Console Installer] : Configuring ZSH..."
     chsh -s "$(command -v zsh)"
-    echo "----------------------"
+    echo "[Console Installer] : ----------------------"
 
-    echo "+ Installation Console successful! (Restarting the computer to use Zsh for the first time) +"
+    echo ""
+    echo "[Console Installer] : + Installation Console successful! (Restarting the computer to use Zsh for the first time) +"
 }
 
 main "$@"
