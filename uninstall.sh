c78e7644c17aacb91edec6c551a60a3b94c63049
Arturo Nava <arthurnavah@gmail.com>
2022-04-03 00:25:38 -0500

    config: uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-sudo apt remove -y --purge zsh tmux alacritty vim vim-editor neovim nvim
-sudo snap remove --purge zsh tmux alacritty vim vim-editor neovim nvim
+sudo apt remove -y --purge zsh
+sudo apt remove -y --purge tmux
+sudo apt remove -y --purge alacritty
+sudo apt remove -y --purge vim
+sudo apt remove -y --purge vim-editor
+sudo apt remove -y --purge neovim
+sudo apt remove -y --purge nvim
+
+sudo snap remove --purge zsh 
+sudo snap remove --purge tmux
+sudo snap remove --purge alacritty
+sudo snap remove --purge vim
+sudo snap remove --purge vim-editor
+sudo snap remove --purge neovim
+sudo snap remove --purge nvim
 
 rm -rf ~/.config/lvim
 rm -rf ~/.config/nvim
